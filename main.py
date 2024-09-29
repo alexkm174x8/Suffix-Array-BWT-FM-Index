e273c79e5f84af92fc8f0e07afd4f55edf81d981
Alejandro Kong <115722702+alexkm174x8@users.noreply.github.com>
2024-09-28 18:22:51 -0600

    Done Suffix Array

--- main.py
+++ main.py
@@ -10,54 +10,6 @@ def read_text_file(filename):
     with open(filename, 'r') as file:
         return file.read()
 
-def build_c_array(bwt):
-    c = {}
-    sorted_bwt = sorted(bwt)
-    for char in set(bwt):
-        c[char] = sorted_bwt.index(char)
-    return c
-
-def build_occur_table(bwt):
-    occur = defaultdict(lambda: [0] * (len(bwt) + 1))
-    for i in range(1, len(bwt) + 1):
-        char = bwt[i - 1]
-        for c in occur:
-            occur[c][i] = occur[c][i - 1] + (1 if c == char else 0)
-        occur[char][i] = occur[char][i - 1] + 1
-    return occur
-
-def build_bwt(text, suffix_array):
-    return ''.join([text[i - 1] for i in suffix_array])
-
-def inverse_bwt(bwt):
-    c = build_c_array(bwt)
-    occur = build_occur_table(bwt)
-    original = []
-    index = 0 
-    
-    for _ in range(len(bwt)):
-        char = bwt[index]
-        original.append(char)
-        index = c[char] + occur[char][index]
-    
-    return ''.join(original[::-1])
-
-def backward_search(bwt, c, occur, pattern):
-    sp = 0
-    ep = len(bwt) - 1
-
-    for char in reversed(pattern):
-        if char not in c:
-            return -1  
-        
-        sp = c[char] + occur[char][sp]
-        ep = c[char] + occur[char][ep + 1] - 1
-        
-        if sp > ep:
-            return -1
-    
-    return sp, ep
-
 if __name__ == "__main__":
     filename = "example.txt"
     text = read_text_file(filename)
