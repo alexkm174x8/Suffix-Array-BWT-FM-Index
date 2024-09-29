2fed5ab42fa3bd91808fa80937ff7213768353d2
mii1rii <mirandaarronizz@gmail.com>
2024-09-28 18:30:00 -0600

    Done with BWT

--- main.py
+++ main.py
@@ -10,12 +10,55 @@ def read_text_file(filename):
     with open(filename, 'r') as file:
         return file.read()
 
+def build_c_array(bwt):
+    c = {}
+    sorted_bwt = sorted(bwt)
+    for char in set(bwt):
+        c[char] = sorted_bwt.index(char)
+    return c
+
+def build_occur_table(bwt):
+    occur = defaultdict(lambda: [0] * (len(bwt) + 1))
+    for i in range(1, len(bwt) + 1):
+        char = bwt[i - 1]
+        for c in occur:
+            occur[c][i] = occur[c][i - 1] + (1 if c == char else 0)
+        occur[char][i] = occur[char][i - 1] + 1
+    return occur
+
+def build_bwt(text, suffix_array):
+    return ''.join([text[i - 1] for i in suffix_array])
+
+def inverse_bwt(bwt):
+    c = build_c_array(bwt)
+    occur = build_occur_table(bwt)
+    original = []
+    index = 0
+    
+    for _ in range(len(bwt)):
+        char = bwt[index]
+        original.append(char)
+        index = c[char] + occur[char][index]
+    
+    return ''.join(original[::-1])
+
 if __name__ == "__main__":
     filename = "example.txt"
     text = read_text_file(filename)
 
     if not text.endswith('$'):
         text += '$'
-
+        
     suffix_array = build_suffix_array(text)
     print("Suffix Array:", suffix_array)
+    
+    bwt = build_bwt(text, suffix_array)
+    print("BWT:", bwt)
+
+    c = build_c_array(bwt)
+    occur = build_occur_table(bwt)
+    print("C array:", c)
+    print("Occur table:", dict(occur))
+
+
+
